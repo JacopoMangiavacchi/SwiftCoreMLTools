3bef08957703d74548207bda0ea21a4a4874d287
Jacopo Mangiavacchi <jamangia@microsoft.com>
2020-01-30 00:02:35 -0800

    Add model with neuralnetwork preliminary tests

--- Tests/SwiftCoreMLToolsTests/ModelTests.swift
+++ Tests/SwiftCoreMLToolsTests/ModelTests.swift
@@ -37,9 +37,29 @@ final class ModelTests: XCTestCase {
         XCTAssertEqual(model.items?.count, 4)
     }
 
+    func testWithNeuralNetwork() {
+        let model = Model(version: 4,
+                          shortDescription: "Trivial linear classifier",
+                          author: "Jacopo Mangiavacchi",
+                          license: "MIT",
+                          userDefined: ["SwiftCoremltoolsVersion" : "0.1"]) {
+            Input(name: "dense_input", shape: [1], featureType: .Double)
+            Output(name: "output", shape: [1], featureType: .Double)
+            TrainingInput(name: "dense_input", shape: [1], featureType: .Double)
+            TrainingInput(name: "output_true", shape: [1], featureType: .Double)
+            NeuralNetwork {
+                InnerProductLayer(name: "layer1")
+                InnerProductLayer(name: "layer2")
+            }
+        }
+
+        XCTAssertEqual(model.items?.count, 5)
+    }
+
     static var allTests = [
         ("testSingleInputs", testSingleInputs),
         ("testMultipleInputs", testMultipleInputs),
         ("testWithMetadata", testWithMetadata),
+        ("testWithNeuralNetwork", testWithNeuralNetwork),
     ]
 }
